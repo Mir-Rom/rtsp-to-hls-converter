8690afd155678b07db75cf28bafd784ce2a9463f
Mir-Rom <rborisovic74@gmail.com>
2025-09-06 20:48:09 +0300

    convertion fix wip

--- app.ts
+++ app.ts
@@ -2,7 +2,14 @@ import express from 'express'
 import cors from 'cors'
 import router from './route.ts'
 import { converter } from './controller.ts'
-import { API_BASE, HLS_FRAGMENTS_PATH, RTSP_URL } from './config.ts'
+import fs from 'node:fs'
+import path from 'node:path'
+import {
+	API_BASE,
+	HLS_FRAGMENTS_PATH,
+	HLS_PLAYLIST_PATH,
+	RTSP_URL,
+} from './config.ts'
 const app = express()
 
 app.disable('x-powered-by')
@@ -13,7 +20,17 @@ app.use(
 		credentials: true,
 	})
 )
+
+process.on('SIGINT', () => {
+	const hlsPath = path.join(import.meta.dirname, 'hls')
+	if (fs.existsSync(hlsPath)) {
+		fs.rmSync(hlsPath, { recursive: true, force: true })
+	}
+	process.exit(0)
+})
+
 app.use(`${API_BASE}/files`, express.static(HLS_FRAGMENTS_PATH))
+app.use(`${API_BASE}/hls/hls_fragments/`, express.static(HLS_FRAGMENTS_PATH))
 app.use('/', router)
 if (!RTSP_URL) {
 	console.log('Please fill in .env according to an example')
--- controller.ts
+++ controller.ts
@@ -4,8 +4,8 @@ import path from 'node:path'
 import {
 	HLS_PATH,
 	HLS_FRAGMENTS_PATH,
-	RTSP_URL,
 	HLS_PLAYLIST_PATH,
+	STATIC_PATH_TO_FILES,
 } from './config.ts'
 if (!fs.existsSync(HLS_PATH)) {
 	fs.mkdirSync(HLS_PATH), { recursive: true }
@@ -16,34 +16,49 @@ if (!fs.existsSync(HLS_FRAGMENTS_PATH)) {
 export function converter(RTSP_URL: string, noAudio = true) {
 	const ffmpegCommand = ffmpeg()
 		.input(RTSP_URL)
+		.inputOption('-ss 3')
 		.inputOptions([
 			'-hide_banner',
 			'-rtsp_transport tcp',
 			'-use_wallclock_as_timestamps 1',
 			'-fflags +genpts',
+			'-strict 5',
 			'-fflags discardcorrupt',
 			'-loglevel debug',
 			'-flags low_delay',
 			'-fflags latm',
 			'-fflags nobuffer',
+			'-probesize 32M',
+			'-analyzeduration 0',
+			'-r:0 12',
 		])
 		.outputOption(
 			'-hls_segment_filename',
-			path.join(HLS_FRAGMENTS_PATH, 'data%03d.ts')
+			path.join(HLS_FRAGMENTS_PATH, 'data%03d.m4s')
 		)
 		.toFormat('hls')
 		.outputOptions([
-			'-c:v libx264',
+			'-c:v copy',
 			'-preset ultrafast',
 			'-tune zerolatency',
-			'-skip_frame no_skip',
-			'-hls_flags delete_segments+temp_file',
-			'-hls_time 10',
-			'-g 75',
-			'-keyint_min 75',
+			'-g 18',
+			'-keyint_min 18',
+			'-sc_threshold 0',
+			'-hls_time 1.5',
+			'-hls_list_size 4',
+			'-hls_flags delete_segments+append_list+omit_endlist+independent_segments',
+			'-hls_delete_threshold 2',
+			'-flush_packets 1',
+			'-max_delay 0',
+			'-avioflags direct',
+			'-hls_segment_type fmp4',
+			'-hls_init_time 1',
+			`-hls_fmp4_init_filename hls/hls_fragments/init.mp4`,
+			`-hls_base_url ${STATIC_PATH_TO_FILES}/`,
 		])
 		.output(HLS_PLAYLIST_PATH)
 		.on('start', (cmd) => console.log('FFmpeg command:', cmd))
+		// .on('codecData', (data) => console.log('CodecData', data))
 		.on('stderr', (line) => {
 			console.log(line)
 		})
